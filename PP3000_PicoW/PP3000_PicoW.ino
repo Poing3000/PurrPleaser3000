e6fd7abf2c209438aa8119d0b99f72795404118c
Poing3000 <85301862+Poing3000@users.noreply.github.com>
2024-04-29 11:16:49 +0100

    Init

---
+++ PP3000_PicoW/PP3000_PicoW.ino
@@ -0,0 +1,27 @@
+/*
+ * Name:	PurrPLeaser3000
+ * Author:	Poing3000
+ * Status:	Dev
+ *
+ * Description:
+ * This code is for the PurrPleaser3000.
+ * It is built for a Raspberry Pico W and a TMC2209 stepper driver.
+ * Further info at: https://github.com/Poing3000/PurrPleaser3000
+ *
+ * Updates - DEV:
+ * [ ] - Implement FP3000
+ * [ ] - Implement Control
+ * [ ] - Implement HMI
+ *		[ ] - Implement MQTT
+ *		[ ] - WebInterface
+ */
+
+
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# PurrPleaser3000
+<img src="Logo_PurrPleaser.jpg" width="200">
+ Automatic cat feeder - Der Katzenfutterautomat
